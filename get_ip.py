33b0386013f34d6cd6c9bbb6016fe3660c7b5c0a
Amol Deodhar <adeodhar@akamai.com>
2018-08-21 18:42:57 -0400

    question/answer

--- get_ip.py
+++ get_ip.py
@@ -6,7 +6,13 @@ import json
 log = Logger().log_to_file(__file__)
 
 
-for each in json.loads(requests.get("http://jsonplaceholder.typicode.com/posts/").content.decode('UTF-8')):
+# for each in json.loads(requests.get("http://jsonplaceholder.typicode.com/posts/").content.decode('UTF-8')):
+#     for key, value in each.items():
+#         if key == "title" and value == "nesciunt quas odio":
+#             log.info("Content: %s", each)
+
+for each in json.loads(requests.get("http://polls.apiblueprint.org/questions").content.decode('UTF-8')):
+    log.info("------------------------")
     for key, value in each.items():
-        if key == "title" and value == "nesciunt quas odio":
-            log.info("Content: %s", each)
+        if key == "question":
+            log.info("Question: %s", value)
